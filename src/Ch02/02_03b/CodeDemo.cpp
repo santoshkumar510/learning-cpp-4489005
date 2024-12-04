197666a7eef870ed376892f4b9e2cc413e1d30e8
santoshkumar510 <santoshapsce@gmail.com>
2024-12-04 19:34:23 +0000

    Practice

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,18 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
+    bool my_flag;
     std::cout << "Hi There!" << std::endl;
-    
+
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "my_flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
     std::cout << std::endl << std::endl;
+    
     return (0);
 }
--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,16 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cstdint>
+
+#define CAPACITY 5000
 
 int main(){
-    
+
+    int32_t large = CAPACITY;
+    uint8_t small = 37;
+    large += small;
+        
     std::cout << std::endl << std::endl;
     return (0);
 }
